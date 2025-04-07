86cf6f5e49c8b709233123379d9626785a7307f8
rica <rica99999@gmail.com>
2025-04-07 15:53:24 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+# ******************* FILE PATH ***********************************
+
+FILEPATH = r"C:\Users\User\Documents\python\PyCharnProjects\todo_app\todos.txt"
+
+# ******************* functions ***********************************
+def get_todos(filepath_local=FILEPATH):
+    """Read the text file and return the lis of to-do items."""
+    with open(filepath_local, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath_local1=FILEPATH):
+    """Write the to-do list of items in the text file."""
+    with open(filepath_local1, "w") as file:
+        file.writelines(todos_arg)
+
+
+
+if __name__=="__main__":
+    print("Hello from functions")
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from streamlit import session_state
+
+import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"] +"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    todo = todo.title()
+    todo = f"{todo.strip('\n')}"
+    #st.checkbox(row)
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="Enter a todo:",
+              placeholder="Add a new todo..",
+              on_change=add_todo, key="new_todo")
+# print("Hello")
+# session_state
